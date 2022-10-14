be1029a1da14bd36ae621c2803a514ff2bf73681
Eduard22222 <115102987+Eduard22222@users.noreply.github.com>
2022-10-14 15:32:14 +0300

    Лабораторная работа 3

---
+++ Лаборатоная работа 3/task2.py
@@ -0,0 +1,11 @@
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+max_i = list_numbers[0]
+for current in list_numbers:
+    if current > max_i:
+        max_i= current
+
+a = list_numbers.index(max_i)
+list_numbers[a], list_numbers[-1] = list_numbers[-1], list_numbers[a]
+
+print(list_numbers)
